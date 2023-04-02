fcc657204efbc864e5d06c300ad5e8542b867493
amansingh04 <aman.singh04@nagarro.com>
2023-04-02 16:57:05 +0530

    After Episode3

--- app.js
+++ app.js
@@ -2,13 +2,47 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
-const parent = React.createElement("div",
-    { id: "parent" },
-    React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]
-    )
-);
+// React createElement => Object => HTML Element (when rendered to DOM)
+
+const heading = React.createElement("h1", {id: 'heading'}, "Namaste React");
+
+// JSX = is not HTML || HTML Like Syntax or XML Like
+//JSX (transpiled before it reaches the JS) -PARCEL
+// JSX => React.createElement => JS Object => HTML Element (when rendered to DOM)
+// Babel is responsible to transpile are code
+const Title = () => <h1 id="heading">Namaste React Using JSX</h1>;
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+//React Component
+// Class Based Component - OLD
+// Functional Component -  NEW
+
+// React Function Component
+//Component Composition
+const HeadingComponent1 = () => {
+    return (
+        <React.Fragment>
+            <Title></Title>
+            <React.Fragment>
+                <h3>Inside Second Fragment1</h3>
+            </React.Fragment>
+        </React.Fragment>
+    );
+}; 
+
+const HeadingComponent2 = () => {
+    return (
+        <React.Fragment>
+            <Title></Title>
+            <React.Fragment>
+                <h3>Inside Second Fragment2</h3>
+            </React.Fragment>
+        </React.Fragment>
+    );
+}; 
+
+const root1 = ReactDOM.createRoot(document.getElementById('root'));
+const root2 = ReactDOM.createRoot(document.getElementById('root'));
+
+root1.render(<HeadingComponent1 />);
+root2.render(<HeadingComponent2 />);
